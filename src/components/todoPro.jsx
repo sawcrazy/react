c95822ebe12b2f1d45300855dd82c3656c433389
sawcrazy <sawvrazy@yandex.ru>
2023-03-11 23:40:05 +0300

    check Vlad

--- src/components/todoPro.jsx
+++ src/components/todoPro.jsx
@@ -4,23 +4,46 @@ class TodoPro extends React.Component {
     state = {
         arr:[{
             id:0,
-            task:'задача',
+            task:'',
         }],
+        input: '',
     }
     inputValue = (event) =>{
         let input = event.target.value;
-        this.setState({arr: [{
-            task:input,
-            }]})
+        this.setState({input:input})
+
     }
     addValue = () =>{
+        let id = this.state.arr.length;
         let arrItem = {
-            id:this.state.arr.length ? this.state.arr[this.state.arr.length - 1] + 1 : 1,
-            task: this.state.arr[0].task,
+            id: id,
+            task: this.state.input,
         };
         let newArr = [...this.state.arr,arrItem]
         this.setState({arr: newArr})
         console.log(newArr)
+    }
+    renderList = () =>{
+
+       return  this.state.arr.map((item) =>{
+           if(item.task !== ''){
+               return(
+                   <li key={item.id}>
+                       {item.task}
+                       <button onClick={()=>{this.deleteList(item.id)}}>delete</button>
+                   </li>
+
+               )
+           }
+           return
+
+        })
+
+    }
+    deleteList =(id) =>{
+        let arr = this.state.arr.filter((item,) => item.id !== id);
+
+        this.setState({arr: arr})
     }
 
 
@@ -32,9 +55,13 @@ class TodoPro extends React.Component {
                 <div>
                     <input onChange={this.inputValue}/>
                     <button onClick={this.addValue}>Добавить</button>
-                    <div>{this.state.arr[0].task}</div>
+                    <div>{this.state.input}</div>
+                    <div>
+                        <ul>
+                            {this.renderList()}
+                        </ul>
+                    </div>
                 </div>
-                {this.state.arr[0].id}
             </div>
         )
     }
